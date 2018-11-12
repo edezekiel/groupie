7994dd93cf010855bd414f7254c59bdb88811120
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2018-11-12 17:01:41 -0500

    fix and run migrations

--- db/migrate/01_create_bands.rb
+++ db/migrate/01_create_bands.rb
@@ -2,8 +2,8 @@ class CreateBands < ActiveRecord::Migration[5.0]
   def change
     create_table :bands do |table|
       table.string :name
-      table.genre :string
-      table.num_of_members :integer
+      table.string :genre
+      table.integer :num_of_members
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2) do
+
+  create_table "bands", force: :cascade do |t|
+    t.string  "name"
+    t.string  "genre"
+    t.integer "num_of_members"
+  end
+
+  create_table "concerts", force: :cascade do |t|
+    t.string  "title"
+    t.string  "location"
+    t.integer "price"
+  end
 
 end
