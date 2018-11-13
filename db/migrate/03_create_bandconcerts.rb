419db496cc9cbf802549e3a21afed5e574d4a32a
paristuzun <paristuzun@gmail.com>
2018-11-13 13:23:27 -0500

    added bandconcerts db

--- db/migrate/03_create_bandconcerts.rb
+++ db/migrate/03_create_bandconcerts.rb
@@ -1,9 +1,9 @@
 class CreateBandConcerts < ActiveRecord::Migration[5.0]
   def change
     create_table :bandconcerts do |table|
-      #TODO: fill in the band and concert ids
-      table.integer
-      table.integer
-    end
+
+      table.integer :band_id
+      table.integer :concert_id
+     end
   end
 end
